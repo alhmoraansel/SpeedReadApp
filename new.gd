774288849a41fd1f1b7f127d5cb5d8cf7eed7024
Alhmora Ansel <109255259+alhmoraansel@users.noreply.github.com>
2024-02-11 08:06:49 +0530

    fixed bugs

--- new.gd
+++ new.gd
@@ -10,11 +10,11 @@ extends Control
 @onready var texts = %texts
 @onready var progress_bar = %ProgressBar
 @onready var time_left = %timeLeft
-
-var is_playing := false
+@onready var timerupdatetimeleft = %timerupdatetimeleft
+@onready var wordcountlabel = %wordcountlabel
 
 var filePath
-var main_string = null
+var main_string :String
 var word_count=0.0
 var z:=0.0
 var string_to_print
@@ -23,7 +23,8 @@ var wordsperminte = 60
 var timeLeft
 
 func _on_timer_timeout():
-	if string_to_print: label.text = string_to_print
+	if string_to_print:
+		label.text = string_to_print
 	progress_bar.value =progress
 	if main_string: next_word(main_string,z)
 	if z-1 == word_count:
@@ -32,12 +33,12 @@ func _on_timer_timeout():
 		pausebutton.visible = false
 		z=0
 		timer.stop()
-	updateTimeLeftIndicator()
 
 func next_word (text:String,index:int):
 	string_to_print = text.get_slice(" ",index)
 	z+=1
 	progress =  z/word_count *100
+	updateWordCount()
 
 func _on_h_slider_value_changed(value):
 	wordsperminte = 60/timer.wait_time
@@ -46,22 +47,40 @@ func _on_h_slider_value_changed(value):
 	updateTimeLeftIndicator()
 
 func _on_file_dialog_file_selected(path):
+	texts.visible = true
 	filepathlabel.text = path
 	filePath = path
 	if filePath:
 		var file  = FileAccess.open(filePath,FileAccess.READ)
-		var text  =  file.get_as_text()
-		main_string = text
+		main_string  =  file.get_as_text()
 		texts.text = main_string
-		word_count = main_string.get_slice_count(" ")
-	updateTimeLeftIndicator()
+		format_string()
+		updateTimeLeftIndicator()
+		updateWordCount()
+	timerupdatetimeleft.start()
+
+func format_string():
+	if main_string.contains("\n"):
+		main_string =  main_string.replace("\n"," ")
+	if main_string.contains("\r"):
+		main_string =  main_string.replace("\r"," ")
+	if main_string.contains(","):
+		main_string = main_string.replace(","," ")
+	if main_string.contains("."):
+		main_string =  main_string.replace("."," ")	
+	if main_string.contains(";"):
+		main_string =  main_string.replace(";"," ")	
+	word_count = main_string.get_slice_count(" ")
 
 func _on_pausebutton_button_down():
 	resumebutton.visible = true
 	pausebutton.visible = false
+	texts.visible = true
 	timer.paused = true
+	timerupdatetimeleft.stop()
 
 func _on_resumebutton_button_down():
+	format_string()
 	if main_string == null:
 		file_dialog.popup()
 	pausebutton.visible = true
@@ -69,14 +88,17 @@ func _on_resumebutton_button_down():
 	texts.visible = false
 	timer.paused = false
 	timer.start()
+	timerupdatetimeleft.start()
 
 func _on_stopbutton_button_down():
 	progress = 0.0
+	z=0
 	texts.visible = true
 	resumebutton.visible = true
 	pausebutton.visible = false
-	z=0
 	timer.stop()
+	timerupdatetimeleft.stop()
+	updateWordCount()
 	
 func _on_startbutton_button_down():
 	resumebutton.visible = true
@@ -85,17 +107,20 @@ func _on_startbutton_button_down():
 	z=0
 	file_dialog.popup()
 
-func updateTimeLeftIndicator():
-	if word_count:
-		timeLeft = (word_count-z)/wordsperminte
-		if timeLeft < 1.0:
-			timeLeft*=60
-			time_left.text = str(snapped(timeLeft,1))+"  seconds Left"
-		else:
-			time_left.text = str(snapped(timeLeft,0.1))+"  mins Left"
-
 func _on_texts_text_changed():
-	updateTimeLeftIndicator()
 	if texts.visible:
 		main_string=texts.text
 		word_count = main_string.get_slice_count(" ")
+		updateTimeLeftIndicator()
+
+func updateTimeLeftIndicator():
+	if word_count:
+		timeLeft = (word_count-z)/wordsperminte
+		time_left.text =str(int(timeLeft)) +" mins  " + str(snapped((timeLeft - int(timeLeft))*60,1))+" seconds"
+
+func updateWordCount():
+	if word_count:
+		wordcountlabel.text = str(word_count - z)+" words left"
+
+func _on_time_left_timeout():
+	updateTimeLeftIndicator()
